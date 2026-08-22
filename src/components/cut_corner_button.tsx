5553e40dd4cfe37bba4f6fe016eaf78aa5b90092
protzzz <99717913+protzzz@users.noreply.github.com>
2026-08-22 09:58:55 +0200

    Restore cut corner button overlay

--- src/components/cut_corner_button.tsx
+++ src/components/cut_corner_button.tsx
@@ -10,10 +10,10 @@ export const CutCornerButton = (props: ComponentPropsWithoutRef<'button'>) => {
         className,
       )}
     >
-      {/* <div className="absolute inset-0 mask-[linear-gradient(225deg,transparent,transparent_10px,black_10px)] outline-2 -outline-offset-2 outline-fuchsia-500 outline-solid"></div> */}
+      <div className="absolute inset-0 mask-[linear-gradient(225deg,transparent,transparent_10px,black_10px)] outline-2 -outline-offset-2 outline-fuchsia-500 outline-solid"></div>
 
       {/* Just for ResposivelyApp for version control */}
-      <div className="absolute inset-0 mask-[linear-gradient(225deg,transparent,transparent_10px,black_10px)] outline-2 -outline-offset-2 outline-fuchsia-500 outline-solid [-webkit-mask-image:linear-gradient(225deg,transparent,transparent_10px,black_10px)]"></div>
+      {/* <div className="absolute inset-0 mask-[linear-gradient(225deg,transparent,transparent_10px,black_10px)] outline-2 -outline-offset-2 outline-fuchsia-500 outline-solid [-webkit-mask-image:linear-gradient(225deg,transparent,transparent_10px,black_10px)]"></div> */}
 
       <svg
         width="24"
@@ -23,7 +23,7 @@ export const CutCornerButton = (props: ComponentPropsWithoutRef<'button'>) => {
         xmlns="http://www.w3.org/2000/svg"
         className="absolute top-0 right-0 text-fuchsia-500"
       >
-        <path d="M0 1H12.2667L23 11.7333V24" stroke="currentColor" stroke-width="2"></path>
+        <path d="M0 1H12.2667L23 11.7333V24" stroke="currentColor" strokeWidth="2"></path>
       </svg>
       <span className="leading-6">{children}</span>
     </button>
